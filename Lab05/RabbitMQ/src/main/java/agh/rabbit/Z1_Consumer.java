0cc947415ae7bc476783958a762efab9f053e066
pswierzy <pswierzy@student.agh.edu.pl>
2026-04-22 17:48:41 +0200

    lab05 finished

---
+++ Lab05/RabbitMQ/src/main/java/agh/rabbit/Z1_Consumer.java
@@ -0,0 +1,59 @@
+package agh.rabbit;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import java.io.IOException;
+
+public class Z1_Consumer {
+
+    public static void main(String[] argv) throws Exception {
+
+        // info
+        System.out.println("Z1 CONSUMER");
+
+        // connection & channel
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.basicQos(1);
+
+        // queue
+        String QUEUE_NAME = "queue1";
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        // consumer (handle msg)
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println("Received: " + message);
+
+                try {
+                    int timeToSleep = Integer.parseInt(message);
+                    Thread.sleep(timeToSleep * 1000L);
+                } catch (Exception e) {
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+                channel.basicAck(envelope.getDeliveryTag(), false);
+            }
+        };
+
+        // start listening
+        System.out.println("Waiting for messages...");
+        channel.basicConsume(QUEUE_NAME, false, consumer);
+
+        // close
+//        channel.close();
+//        connection.close();
+    }
+}
